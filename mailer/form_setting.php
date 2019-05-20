52bd71e23d6b96f3fd44ec1ab3ad098c89a80893
Felipe Romero <feliperom87@gmail.com>
2019-05-20 11:47:11 -0300

    New Site

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from the site 'Felipe Romero'"; //letter subject
+	$to = 'feliperom87@gmail.com'; // Enter Your E-mail
+	$from = 'admin@feliperomero.com.br'; // Admin e-mail
+	$fromName = 'Felipe Romero'; // Your company name
+	$charset = 'UTF-8';
+?>
